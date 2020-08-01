da20d85867021dc75ec537ba67b7f94400d50418
PrashantDandriyal <prashantdandriyal7@gmail.com>
2020-08-01 18:17:14 +0530

    Correct OSI info

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MPL License",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
